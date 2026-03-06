9319c9ea5d9ab0e24be2aa597a6998b16870625b
Gregory Gay <gregory.gay@diamond.ac.uk>
2026-03-06 16:39:19 +0000

    add startup.sh script with doWait completion check

---
+++ template/ioc/startup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+TOP=/epics/ioc
+cd ${TOP}
+CONFIG_DIR=${TOP}/config
+
+set -ex
+
+TMP_DIR=/tmp
+CONFIG_DIR=/epics/ioc/config
+THIS_SCRIPT=$(realpath ${0})
+override=${CONFIG_DIR}/startup.sh
+
+# 'startup.sh' may be overriden in the ioc/config directory
+if [[ -f ${override} && ${override} != ${THIS_SCRIPT} ]]; then
+    exec bash ${override}
+fi
+
+# confirm that the k8s ioc startup check is enabled; if not just exit successfully
+if [[ ${K8S_IOC_STARTUP_ENABLED} != 'true' ]]; then
+    echo "Startup check for ${IOC_NAME} currently disabled" > /proc/1/fd/1
+    exit 0
+fi
+
+# check that the ibek doWait command has completed successfully
+if [ -f ${TMP_DIR}/doWait_completed.txt ]; then
+    echo "k8s startup check for ${IOC_NAME}: ibek doWait command completion validated" > /proc/1/fd/1
+    exit 0
+else
+    exit 1
+fi
